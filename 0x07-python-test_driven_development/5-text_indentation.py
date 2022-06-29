7097240199599cfca1ed3adddf118db90b88473e
Onielcares <Dehinbonatt@gmail.com>
2022-06-29 07:34:23 -0700

    TDD 2matrix correction

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""
-Prints a text with 2 new lines after each of these characters: ., ? and :
-"""
+# 5-text_indentation.py
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: ., ? ..:
-        Args:
-            text (str): text
+    """Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for ch in text:
-        if ch == "." or ch == "," or ch == "?" or ch == ":":
-            print("\n")
-        else:
-            print(ch, end="")
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
