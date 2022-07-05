7eb92733c7c79e526bad71385a034c946ceb913e
Onielcares <Dehinbonatt@gmail.com>
2022-07-05 16:17:39 -0700

    Input and Output Python

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""contains a class"""
+
+"""Defines a Student class"""
 
 
 class Student:
-    """student class"""
+    """A Student class"""
 
-    def __int__(self, first_name, last_name, age):
-        """initialization of student object"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student object
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns a dictionary representation of a
-           student class
-        """
+        """Retrieves a dictionary representation of a Student instance"""
         return self.__dict__
