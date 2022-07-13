98c7e5f60e283ea1955e7ccf48206c44bd90f0f4
root <dehinbonatt@gmail.com>
2022-07-12 17:03:03 -0700

    glory

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-
+# test_square.py
 """Defines unittests for models/square.py.
 Unittest classes:
     TestSquare_instantiation - line 24
@@ -653,7 +652,6 @@ class TestSquare_to_dictionary(unittest.TestCase):
         s = Square(10, 10, 10, 10)
         with self.assertRaises(TypeError):
             s.to_dictionary(1)
-
 
 if __name__ == "__main__":
     unittest.main()
