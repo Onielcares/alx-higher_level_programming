49ec1c1bc511216637c9605fb9717c176085fc0e
Onielcares <dehinbonatt@gmail.com>
2022-09-22 21:10:25 +0000

    update body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get The body  size  from outgoing  request to url introduced as argument
-curl -s "$1" | wc -c
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response using curl
+curl -sI "$1" | awk '/Content-Length/{print $2}'
