779d3276a7a0d8fde32a611d852e4c46a67d69f8
Onielcares <dehinbonatt@gmail.com>
2022-09-22 21:45:57 +0000

    updating 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL and sends a POST request to the URL
-curl -sL -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
+curl -sL -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
