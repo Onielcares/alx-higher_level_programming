93f7c6790922da306f4e186bb1dc3bd9eb187da5
Onielcares <dehinbonatt@gmail.com>
2022-09-22 21:37:12 +0000

    correcting 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-"""
-find the peek
-"""
+"""function that finds a peak in a list of unsorted integers."""
 
 
-def find_peak(arr):
-    """
-        find the peek
-    """
-    if arr == []:
+def find_peak(list_of_integers):
+    """finds a peak"""
+    if list_of_integers:
+        peak = list_of_integers[0]
+        for i in list_of_integers:
+            if i > peak:
+                peak = i
+        return peak
+    else:
         return None
-    if len(arr) == 1:
-        return arr[0]
-    if arr[0] >= arr[1]:
-        return arr[0]
-    if arr[len(arr) - 1] >= arr[len(arr) - 2]:
-        return arr[len(arr) - 1]
-    for i in range(1, len(arr) - 1):
-        if arr[i] >= arr[i - 1] and arr[i] >= arr[i + 1]:
-            return arr[i]
